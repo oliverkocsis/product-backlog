2e86622053a6f7794e3416a71c0be9fb8e69a036
Oliver Kocsis <oliver.kocsis@gmail.com>
2022-01-22 13:54:43 +0000

    Create Product Backlog Item button

--- src/ProductBacklog.js
+++ src/ProductBacklog.js
@@ -8,13 +8,19 @@ function ProductBacklog() {
     const example = {
         title: 'Lorem Ipsum',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-        size: 1
+        order: 1,
+        size: 2
     };
 
     const [productBacklogItems, setProductBacklogItems] = useState([{ ...example }, { ...example }]);
 
+    const createProductBacklogItem = function () {
+        setProductBacklogItems([...productBacklogItems, { ...example }]);
+    };
+
     return (
         <div className='ProductBacklog'>
+            <button onClick={createProductBacklogItem}>Create Product Backlog Item</button>
             {productBacklogItems.map((productBacklogItem, index) => (
                 <ProductBacklogItem
                     key={index}
